9cf3f42c7be94d9479087d53935dd5916ef1dcd2
thanaschai <git config --global user.name>
2022-12-13 16:33:36 +0700

    LAB7_2DONE

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,14 +1,32 @@
 #include<iostream>
 
 using namespace std;
-
+ 
 int main(){
+	int i = 0, P[5000], k;
+	cout << "Please input k: ";
+	cin >> k ;
+    while(i < 5000){
+	P[i] = 2*i+1;
+	i++;}
 	
-	int i = 0, P[5000], k, result;
-	while(i < 5000){
-		P[i] = 2*i+1;
+	if (k>0){
+	int i = 0 ;
+	int result = 0 ;
+	while (i < 5000){
+		if (P[i]%k == 0)
+		{ 
+			result = result - P[i] ;
+		}
+		else {
+			result = result + P[i] ;
+			
+		}
 		i++;
+		}
+	cout << "Result = " << result << endl;
+	}else { 
+	cout << "Invalid input!!!";
 	}
-	
 	return 0;
 }
