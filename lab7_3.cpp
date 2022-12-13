08ce865b489812b1b4df34c03ea8a649ed7b152e
thanaschai <git config --global user.name>
2022-12-13 16:52:29 +0700

    LAB7_3DONE

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,15 +1,23 @@
+//การเติมคำตอบในช่องนี้ ขอให้เติมเฉพาะ Function adiff() เท่านั้น โดยห้ามเติม Function main() มาในนี้
 #include<iostream>
 
 using namespace std;
 
-
-int main(){
-  cout << adiff(180,270);
-  cout << adiff(210,45);
-  cout << adiff(0,360);
-  cout << adiff(10,350);
-  cout << adiff(95,260);
-  cout << adiff(90,-90);
-  cout << adiff(1000,280);
-  cout << adiff(60,244);
+int adiff (int A ,int B)
+{ int C ;
+  if (A>B){
+     C = A-B ;
+  }
+  else {
+     C = B-A ;
+  }
+  if (C > 360)
+   {
+    C=C % 360 ;
+   }
+   if (C> 180){
+    C=360-C;
+    }
+  return C ;  
 }
+
